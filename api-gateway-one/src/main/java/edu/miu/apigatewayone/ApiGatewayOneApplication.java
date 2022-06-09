db34ac2a35e03e0b0b1a19fb07bb9c3d18d1a2ec
bijayshr <bijayshrestha.usa@gmail.com>
2022-06-09 13:40:18 -0500

    Created API Gateway

---
+++ api-gateway-one/src/main/java/edu/miu/apigatewayone/ApiGatewayOneApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.apigatewayone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayOneApplication.class, args);
+	}
+
+}
---
+++ api-gateway-one/src/test/java/edu/miu/apigatewayone/ApiGatewayOneApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.apigatewayone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
