973b9bf90e284153fc0eab1f031694923d7806b1
bijayshr <bijayshrestha.usa@gmail.com>
2022-06-09 13:37:38 -0500

    Created eureka-server-one

---
+++ eureka-server-one/src/main/java/edu/miu/eurekaserverone/EurekaServerOneApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.eurekaserverone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerOneApplication.class, args);
+	}
+
+}
---
+++ eureka-server-one/src/test/java/edu/miu/eurekaserverone/EurekaServerOneApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.eurekaserverone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
