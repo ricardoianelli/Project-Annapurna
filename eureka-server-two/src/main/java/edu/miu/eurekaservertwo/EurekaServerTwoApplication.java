e263699ff4e1508bb1544a5368b80354f82eb69f
bijayshr <bijayshrestha.usa@gmail.com>
2022-06-09 15:28:07 -0500

    Updated Eureka Confiurations.

--- eureka-server-two/src/main/java/edu/miu/eurekaservertwo/EurekaServerTwoApplication.java
+++ eureka-server-two/src/main/java/edu/miu/eurekaservertwo/EurekaServerTwoApplication.java
@@ -2,8 +2,10 @@ package edu.miu.eurekaservertwo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerTwoApplication {
 
 	public static void main(String[] args) {
