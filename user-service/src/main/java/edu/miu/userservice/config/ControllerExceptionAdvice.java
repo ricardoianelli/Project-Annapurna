2cb1fcf0fb2293b079d05d99f5d0c4551ead449b
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-10 14:15:38 -0500

    Refactor ControllerExceptionAdvice

--- user-service/src/main/java/edu/miu/userservice/config/ControllerExceptionAdvice.java
+++ user-service/src/main/java/edu/miu/userservice/config/ControllerExceptionAdvice.java
@@ -1,5 +1,6 @@
 package edu.miu.userservice.config;
 
+import edu.miu.userservice.controller.StandardError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,16 +9,34 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import edu.miu.userservice.exception.InputFieldEmptyException;
 import edu.miu.userservice.exception.UserNotFoundException;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
 @ControllerAdvice
 public class ControllerExceptionAdvice {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUsersNotFound(UserNotFoundException userNotFound) {
-        return new ResponseEntity<String>("No User Found", HttpStatus.NOT_FOUND);
+    public ResponseEntity<StandardError> handleUsersNotFound(UserNotFoundException ex, HttpServletRequest request) {
+        String errorMsg = "User not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        return getResponseError(errorMsg, status, request);
     }
 
     @ExceptionHandler(InputFieldEmptyException.class)
-    public ResponseEntity<String> handleInputFieldEntity(InputFieldEmptyException inputFieldEmpty) {
-        return new ResponseEntity<>("Input Field is Empty", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<StandardError> handleInputFieldEntity(InputFieldEmptyException ex, HttpServletRequest request) {
+        String errorMsg = "Input Field is Empty";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return getResponseError(errorMsg, status, request);
     }
+
+    private ResponseEntity<StandardError> getResponseError(String message, HttpStatus status, HttpServletRequest request)  {
+        StandardError error = new StandardError(Instant.now(), status.value(), message, null, request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    private ResponseEntity<StandardError> getResponseError(String message, HttpStatus status, HttpServletRequest request, Exception ex)  {
+        StandardError error = new StandardError(Instant.now(), status.value(), message, ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
 }
