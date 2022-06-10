183097d2954eafb6e1ac0db19fd897709f8096a3
bijayshr <bijayshrestha.usa@gmail.com>
2022-06-09 21:47:17 -0500

    Created auth-server

---
+++ auth-server/src/test/java/edu/miu/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
