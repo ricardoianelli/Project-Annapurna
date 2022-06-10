592c2d1b298eff07d42943026dae02920387a840
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-10 16:25:28 -0500

    Rename WeekdaysRepository to WeekdayRepository

--- etl-service/src/main/java/edu/miu/etlservice/repository/WeekdayRepository.java
+++ etl-service/src/main/java/edu/miu/etlservice/repository/WeekdayRepository.java
@@ -1,9 +1,9 @@
 package edu.miu.etlservice.repository;
 
-import edu.miu.etlservice.model.Weekdays;
+import edu.miu.etlservice.model.Weekday;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WeekdaysRepository extends JpaRepository<Weekdays, Short> {
+public interface WeekdayRepository extends JpaRepository<Weekday, Short> {
 }
