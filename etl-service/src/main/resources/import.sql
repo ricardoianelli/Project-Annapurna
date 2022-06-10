dc5001136555e58199d0335302b4665c3050af0c
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-10 16:27:15 -0500

    Add import.sql

---
+++ etl-service/src/main/resources/import.sql
@@ -0,0 +1,26 @@
+INSERT INTO weekday(id, weekday)
+VALUES (1, 'Sunday'),
+       (2, 'Monday'),
+       (3, 'Tuesday'),
+       (4, 'Wednesday'),
+       (5, 'Thursday'),
+       (6, 'Friday'),
+       (7, 'Saturday');
+
+INSERT INTO dine_type(id, name)
+VALUES (1, 'Breakfast'),
+       (2, 'Brunch'),
+       (3, 'Lunch'),
+       (4, 'Dinner');
+
+INSERT INTO meal(id, name)
+VALUES (1, 'Split Peas Soup'),
+       (2, 'Leafy Greens'),
+       (3, 'Oatmeal'),
+       (4,'Breakfast Potatoes'),
+       (5,'Tofu Scramble'),
+       (6,'Hot Milk'),
+       (7,'Briyani'),
+       (8,'Basmati Rice'),
+       (9,'Dhal'),
+       (10,'Pizza');
