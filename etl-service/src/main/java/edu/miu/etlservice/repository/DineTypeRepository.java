d745141a39e41b3186e82f2dea6d97c92fd70979
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-10 16:26:15 -0500

    Add getDineTypeByName to DineTypeRepository

--- etl-service/src/main/java/edu/miu/etlservice/repository/DineTypeRepository.java
+++ etl-service/src/main/java/edu/miu/etlservice/repository/DineTypeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DineTypeRepository extends JpaRepository<DineType, Short> {
+    DineType getDineTypeByName(String name);
 }
