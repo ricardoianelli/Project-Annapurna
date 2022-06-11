cd2a63c957d1b4da7c7a320759b394f922aaa693
bijayshr <bijayshrestha.usa@gmail.com>
2022-06-11 13:33:47 -0500

    Minor Fix in JwtProperties at loginservice

--- login-service/src/main/java/edu/miu/loginservice/jwt/JwtProperties.java
+++ login-service/src/main/java/edu/miu/loginservice/jwt/JwtProperties.java
@@ -4,9 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties(prefix = "jwt")
-@Data
 @Configuration
+@Data
+@ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
 
 	private String secretKey;
