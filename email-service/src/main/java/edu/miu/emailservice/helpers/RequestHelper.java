26a75da9ac097422b83f79d04b37a4e846481ca2
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-12 15:40:21 -0500

    Add helper to get current HttpRequest

---
+++ email-service/src/main/java/edu/miu/emailservice/helpers/RequestHelper.java
@@ -0,0 +1,16 @@
+package edu.miu.emailservice.helpers;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+public class RequestHelper {
+    public static Optional<HttpServletRequest> getCurrentHttpRequest() {
+        return Optional.ofNullable(RequestContextHolder.getRequestAttributes())
+                .filter(ServletRequestAttributes.class::isInstance)
+                .map(ServletRequestAttributes.class::cast)
+                .map(ServletRequestAttributes::getRequest);
+    }
+}
