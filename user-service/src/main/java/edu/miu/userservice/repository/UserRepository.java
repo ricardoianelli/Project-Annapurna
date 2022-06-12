fefaea4817a7b136c3bcdfb568d188581f2dd26f
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-12 00:03:04 -0500

    Add findBySubscribed to UserRepository

--- user-service/src/main/java/edu/miu/userservice/repository/UserRepository.java
+++ user-service/src/main/java/edu/miu/userservice/repository/UserRepository.java
@@ -4,6 +4,9 @@ import edu.miu.userservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findBySubscribed(boolean subscribed);
 }
