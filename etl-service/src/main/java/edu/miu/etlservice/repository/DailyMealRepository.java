1e02971446b2172d7c68bc7c57482ffbb4883933
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-12 16:50:45 -0500

    Add findDailyMealsByDate repository method

--- etl-service/src/main/java/edu/miu/etlservice/repository/DailyMealRepository.java
+++ etl-service/src/main/java/edu/miu/etlservice/repository/DailyMealRepository.java
@@ -4,6 +4,10 @@ import edu.miu.etlservice.model.DailyMeal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface DailyMealRepository extends JpaRepository<DailyMeal, Long> {
+    List<DailyMeal> findDailyMealsByDate(LocalDate date);
 }
