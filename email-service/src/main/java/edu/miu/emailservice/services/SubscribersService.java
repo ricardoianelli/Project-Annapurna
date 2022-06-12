fa05ae462f8fe6d8ad80bcd6ed1ff9a39000c9f8
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-12 13:22:49 -0500

    Add SubscribersService

--- email-service/src/main/java/edu/miu/emailservice/services/SubscribersService.java
+++ email-service/src/main/java/edu/miu/emailservice/services/SubscribersService.java
@@ -1,9 +1,9 @@
 package edu.miu.emailservice.services;
 
-import edu.miu.emailservice.domain.User;
+import edu.miu.emailservice.dto.UserResponseDTO;
 
 import java.util.List;
 
 public interface SubscribersService {
-    List<User> getSubscribersList();
+    List<UserResponseDTO> getSubscribersList();
 }
