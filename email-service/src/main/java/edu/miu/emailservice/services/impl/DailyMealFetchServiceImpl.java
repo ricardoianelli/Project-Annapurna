9eade9b4ead74f20a568b6be68f070aae1f2fb77
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-12 13:22:06 -0500

    Add DailyMealFetchServiceImpl

---
+++ email-service/src/main/java/edu/miu/emailservice/services/impl/DailyMealFetchServiceImpl.java
@@ -0,0 +1,13 @@
+package edu.miu.emailservice.services.impl;
+
+import edu.miu.emailservice.domain.DailyMeal;
+import edu.miu.emailservice.services.DailyMealFetchService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DailyMealFetchServiceImpl implements DailyMealFetchService {
+    @Override
+    public DailyMeal getDailyMeals() {
+        return null;
+    }
+}
