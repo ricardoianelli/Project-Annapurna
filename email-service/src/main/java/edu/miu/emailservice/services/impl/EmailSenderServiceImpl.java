0ea27141011b1bc063d2407312fd376f1b1b772d
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-12 21:03:12 -0500

    Create email sending logic

--- email-service/src/main/java/edu/miu/emailservice/services/impl/EmailSenderServiceImpl.java
+++ email-service/src/main/java/edu/miu/emailservice/services/impl/EmailSenderServiceImpl.java
@@ -4,10 +4,50 @@ import edu.miu.emailservice.domain.Email;
 import edu.miu.emailservice.services.EmailSenderService;
 import org.springframework.stereotype.Service;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
+
 @Service
 public class EmailSenderServiceImpl implements EmailSenderService {
+
     @Override
     public void send(Email email) {
-        //TODO
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+
+        String email_password = System.getenv("GMAIL_PASSWORD");
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(email.getFrom(), email_password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(email.getFrom(), "Daily Meal Information"));
+            message.setRecipients(
+                    Message.RecipientType.TO, InternetAddress.parse(email.getTo()));
+            message.setSubject(email.getSubject());
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(email.getBody(), "text/html; charset=utf-8");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+
+            message.setContent(multipart);
+            Transport.send(message);
+
+        } catch (Exception ex) {
+            System.out.println("There was an exception sending email: " + ex.getMessage());
+        }
     }
 }
