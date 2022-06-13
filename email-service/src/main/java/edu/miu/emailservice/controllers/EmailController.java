644bc384028f2019f859e6d41035e5fda7ea5350
bijayshr <bijayshrestha.usa@gmail.com>
2022-06-13 12:34:40 -0500

    Added Swagger in Email Service. Refactored API Gateway by removing unnecessary codes.

--- email-service/src/main/java/edu/miu/emailservice/controllers/EmailController.java
+++ email-service/src/main/java/edu/miu/emailservice/controllers/EmailController.java
@@ -1,17 +1,24 @@
 package edu.miu.emailservice.controllers;
 
 import edu.miu.emailservice.facade.EmailServicesFacade;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 import static edu.miu.emailservice.constant.WebResourceKeyConstant.API_V1;
 import static edu.miu.emailservice.constant.WebResourceKeyConstant.UserConstants.EMAIL_BASE;
 
 @RestController
 @RequestMapping(value = API_V1 + EMAIL_BASE)
+@Api(value = "Item Resource to handle all Item related action and queries ")
 public class EmailController {
     final EmailServicesFacade emailServicesFacade;
 
@@ -20,6 +27,13 @@ public class EmailController {
     }
 
     @PostMapping("/send")
+    @ApiOperation(value = "Sends Email to all the subscribed users.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Email Sent Successfully"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "Requested Resource Not Found")
+    })
     public ResponseEntity<?> sendEmails(){
         emailServicesFacade.sendEmailToSubscribers();
         return new ResponseEntity<>(HttpStatus.OK);
