a8eb6bc04442bdbc9b9ff2a7f939be7693d52a36
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-12 20:07:25 -0500

    Define EmailCreatorService interface

--- email-service/src/main/java/edu/miu/emailservice/services/EmailCreatorService.java
+++ email-service/src/main/java/edu/miu/emailservice/services/EmailCreatorService.java
@@ -2,10 +2,9 @@ package edu.miu.emailservice.services;
 
 import edu.miu.emailservice.domain.DailyMeal;
 import edu.miu.emailservice.domain.Email;
-import edu.miu.emailservice.domain.User;
-
-import java.util.List;
+import edu.miu.emailservice.dto.UserResponseDTO;
 
 public interface EmailCreatorService {
-    Email create(User user, DailyMeal dailyMeal);
+    Email getEmail(UserResponseDTO user, String mailBody, String weekday, String date);
+    String getBaseEmailBody(DailyMeal dailyMeal);
 }
