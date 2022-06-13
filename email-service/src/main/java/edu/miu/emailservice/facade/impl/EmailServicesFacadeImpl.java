bd4eda4cf4f84e78d4b98a75a4db09c6b09241bd
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-12 20:06:47 -0500

    Finish implementation to fetch daily meals

--- email-service/src/main/java/edu/miu/emailservice/facade/impl/EmailServicesFacadeImpl.java
+++ email-service/src/main/java/edu/miu/emailservice/facade/impl/EmailServicesFacadeImpl.java
@@ -1,5 +1,6 @@
 package edu.miu.emailservice.facade.impl;
 
+import edu.miu.emailservice.domain.DailyMeal;
 import edu.miu.emailservice.dto.UserResponseDTO;
 import edu.miu.emailservice.facade.EmailServicesFacade;
 import edu.miu.emailservice.services.DailyMealFetchService;
@@ -27,6 +28,13 @@ public class EmailServicesFacadeImpl implements EmailServicesFacade {
     @Override
     public void sendEmailToSubscribers() {
         List<UserResponseDTO> subscribers = subscribersService.getSubscribersList();
-        System.out.println("Test");
+        DailyMeal dailyMeal = dailyMealFetchService.getDailyMeals();
+        String baseMailBody = emailCreatorService.getBaseEmailBody(dailyMeal);
+        String weekday = dailyMeal.getWeekday();
+        String date = dailyMeal.getDate();
+
+        for (UserResponseDTO subscriber : subscribers) {
+            emailSenderService.send(emailCreatorService.getEmail(subscriber, baseMailBody, weekday, date));
+        }
     }
 }
