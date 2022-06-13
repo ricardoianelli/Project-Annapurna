713f9daaab9bc72764494496ff3a9711f10e3748
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-13 11:31:33 -0500

    Allow internal service-to-service communication

--- user-service/src/main/java/edu/miu/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/edu/miu/userservice/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.net.InetAddress;
+
 import static org.springframework.http.HttpMethod.GET;
 
 /**
@@ -36,12 +38,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        String localIp = InetAddress.getLocalHost().getHostAddress() + "/24";
+        String matcherString = "hasIpAddress('" + localIp + "') or hasIpAddress('127.0.0.1')";
+
         http.csrf().disable();
         http.httpBasic().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/api/v1/login/**", "/api/v1/token/refresh/**").permitAll();
-        http.authorizeRequests().antMatchers(GET, "/api/v1/users").permitAll();
+        http.authorizeRequests().antMatchers(GET, "/api/v1/**").access(matcherString);
         http.authorizeRequests().anyRequest().authenticated();
+
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
