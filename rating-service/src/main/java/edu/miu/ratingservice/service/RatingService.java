d06a35216098755ee0bf4b34098d5360d1aac2ec
wizardofwaz8282 <104391698+wizardofwaz8282@users.noreply.github.com>
2022-06-13 14:05:29 -0500

    Removed commentaries

--- rating-service/src/main/java/edu/miu/ratingservice/service/RatingService.java
+++ rating-service/src/main/java/edu/miu/ratingservice/service/RatingService.java
@@ -1,17 +1,9 @@
 package edu.miu.ratingservice.service;
 
-import java.util.List;
-
 import edu.miu.ratingservice.dto.request.RatingRequestDTO;
 import edu.miu.ratingservice.dto.response.RatingResponseDTO;
 
 public interface RatingService {
-    // List<RatingResponseDTO> getRatingsByMeal(Long id);
-
-    // List<RatingResponseDTO> getRatingsByDay();
-
-    // List<RatingResponseDTO> getRatingsByUser(Long id);
-
     RatingResponseDTO getRatingById(Long id);
 
     String addRating(RatingRequestDTO ratingRequestDTO);
