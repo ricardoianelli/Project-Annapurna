1182c8f4b9b6c70828ea95f65bc0b758e2598040
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-06-12 20:05:17 -0500

    Add helper method to get Auth token from current request

--- email-service/src/main/java/edu/miu/emailservice/helpers/RequestHelper.java
+++ email-service/src/main/java/edu/miu/emailservice/helpers/RequestHelper.java
@@ -7,7 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 public class RequestHelper {
-    public static Optional<HttpServletRequest> getCurrentHttpRequest() {
+
+    public static String getAuthenticationToken() throws IllegalStateException {
+        Optional<HttpServletRequest> originalRequestOptional = getCurrentHttpRequest();
+        if (!originalRequestOptional.isPresent()) {
+            throw new IllegalStateException("There is not any request to get Auth token from.");
+        }
+
+        HttpServletRequest originalRequest = originalRequestOptional.get();
+        return originalRequest.getHeader("Authorization");
+    }
+
+    private static Optional<HttpServletRequest> getCurrentHttpRequest() {
         return Optional.ofNullable(RequestContextHolder.getRequestAttributes())
                 .filter(ServletRequestAttributes.class::isInstance)
                 .map(ServletRequestAttributes.class::cast)
